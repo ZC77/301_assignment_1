83d3c974bc3dd74c749140d9f87a16c35e5cba6e
Zachary Cui <zacharycui2009@gmail.com>
2021-04-05 02:13:01 +1200

    added reheap functionality, conducted testing on insert and reheap

--- MyMinHeap.java
+++ MyMinHeap.java
@@ -21,6 +21,23 @@ public class MyMinHeap implements IMinHeap{
             System.out.println("Usage: MyMinHeap [heap array size] \n Array size over 10000 not accepted.");
         }
         heap.heapArray = new int[heap.heapCapacity];
+
+        heap.insert(5);
+        heap.insert(345);
+        heap.insert(234);
+        heap.insert(1);
+        heap.insert(63);
+        heap.insert(7233);
+        heap.insert(435);
+        heap.insert(214);
+        heap.insert(7134);
+        heap.insert(525);
+
+        for (int i = 0; i < heap.heapArray.length; i++) {
+            System.out.print(heap.heapArray[i] + " ");
+            
+        }
+        System.out.println("");
     }
 
     @Override
@@ -62,15 +79,30 @@ public class MyMinHeap implements IMinHeap{
 
     @Override
     public void reHeap() {
-        // Brand new method required, not built in last year's assignment
+        // Brand new method required, not built in last year's assignment.. (In progress)
+
+        int lastParentIndex = (this.heapSize / 2) - 1; 
+
+        for (int x = lastParentIndex ; x >= 0; x--) {
+
+            if (hasRightChild(lastParentIndex) && getRightChild(lastParentIndex) < this.heapArray[lastParentIndex]) {
+                swap(getRightChildIndex(lastParentIndex), lastParentIndex);
+            }
+            if (hasLeftChild(lastParentIndex) && getLeftChild(lastParentIndex) < this.heapArray[lastParentIndex]) {
+                swap(getLeftChildIndex(lastParentIndex), lastParentIndex);
+            }
+    
+            lastParentIndex--;
+
+        }
         
     }
 
     private void upHeap() {
         int index = this.heapSize - 1;
-        while(hasParent(index) && getParent(index) > heapArray[index]) {
-            swap(getParent(index), index);
-            index = getParent(index);
+        while(hasParent(index + 1) && getParent(index) > heapArray[index]) {
+            swap(getParentIndex(index), index);
+            index = getParentIndex(index);
         }
     }
 
@@ -102,12 +134,12 @@ public class MyMinHeap implements IMinHeap{
     // Boolean check given an index, whether that item in the heap has; parent, leftchild, rightchild
     private boolean hasParent(int index) {return (index / 2) > 0;}
     private boolean hasLeftChild(int index) {return (index * 2) <= this.heapSize;}
-    private boolean hasRightChild(int index) {return (index * 2 + 1) <= this.heapSize;}
+    private boolean hasRightChild(int index) {return ((index + 1) * 2) < this.heapSize;}
 
     //Given an index location, retrieve the value of its; parent, leftchild, rightchild
-    private int getParent (int index) {return heapArray[(index / 2)];}
-    private int getLeftChild(int index) {return heapArray[(2 * index)];}
-    private int getRightChild(int index) {return heapArray[(2 * index + 1)];}
+    private int getParent (int index) {return heapArray[(index - 1) / 2];}
+    private int getLeftChild(int index) {return heapArray[(2 * index) + 1];}
+    private int getRightChild(int index) {return heapArray[(2 * index) + 2];}
 
     // Return index of parent, left or right child index
     private int getParentIndex (int childIndex) {return (childIndex - 1) / 2;}
