ea57b5f82833ebd807a136888860be2e16eb75e1
Zachary Cui <zacharycui2009@gmail.com>
2021-04-05 21:37:41 +1200

    Switched to string for array and using compareTo

--- IMinHeap.java
+++ IMinHeap.java
@@ -1,9 +1,9 @@
 public interface IMinHeap {
     
-    public void insert(int x);
+    public void insert(String x);
     public void remove();
-    public void replace(int replacementItem);
-    public int peek();
+    public void replace(String replacementItem);
+    public String peek();
     public void load();
     public void reHeap();
 
